409eee418dfae7d66b76989e9e96711421958cf2
KDSherry <kieran.d.sherry@gmail.com>
2017-07-06 09:16:36 +0100

    Committed App, Main

--- Cinema_Project/scripts/App.jsx
+++ Cinema_Project/scripts/App.jsx
@@ -1,1 +1,15 @@
-//empty
+import React from 'react';
+/*import main from 'main';*/
+
+
+export default class App extends React.Component{
+	render(){
+		return(
+			<div>	
+				<Header />
+				<main>{this.props.children}</main>
+				<Footer />
+			</div>
+		);
+	}
+}
--- Cinema_Project/scripts/main.js
+++ Cinema_Project/scripts/main.js
@@ -1,1 +1,17 @@
-//empty
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+	<Router history = {browserHistory}>
+		<Route path='/' component={App}>
+			<IndexRoute component={Home}></IndexRoute>
+			<Route path='/ListingsPage' component={ListingsPage}></Route>
+			<Route path='/MovieDetails' component={MovieDetails}>
+				<Route path='/MovieDetails:Showings' component={MovieShowings}>
+			</Route>
+			<Route path='/Booking' component={Booking}></Route>
+			<Route path='/Confirmation' component={Confirmation}></Route>
+		</Route>	
+	</Router>	
+, document.querySelector('#app'));

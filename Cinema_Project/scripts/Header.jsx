7ecbf1fa44ba7755bd7c9a995338013d52728496
amhchiu <physicspianist@gmail.com>
2017-07-06 09:27:18 +0100

    header and navbar v1.0

--- Cinema_Project/scripts/Header.jsx
+++ Cinema_Project/scripts/Header.jsx
@@ -1,1 +1,15 @@
-//empty
+//Version 1.0
+import React from 'react';
+
+export default class Header extends React.Component{
+    render(){
+        return(
+            <div className="static-nav">
+                <!--can use title function rather than h1-->
+                <h1>QA CINEMA BLUE</h1>    
+                <NavBar /> 
+            </div>
+        );
+    }
+
+}
--- Cinema_Project/scripts/Navbar.jsx
+++ Cinema_Project/scripts/Navbar.jsx
@@ -1,1 +1,17 @@
-//empty
+import React from 'react';
+import ReactDom from 'react-dom';
+import {IndexLink, Link} from 'react-router';
+
+export function MainNav(){
+    return(
+        <nav className="top-nav-center">
+            <ul className="nav navbar-nav">
+                <li><IndexLink className="pageLink" to="/">Home</IndexLink></li>
+<li><IndexLink className="pageLink" to="/Listing_Gallery">Listing Gallery</li>
+<li><IndexLink className="pageLink" to="/Booking">Booking</IndexLink></li>
+
+            </ul>
+        </nav>
+    );
+}
+

ee486092dba027ada50684515dc85c70966b246e
groganmatt12 <groganmatt@hotmail.co.uk>
2017-07-10 17:23:29 +0100

    Matt: movie details page rendering

--- Cinema_Project/scripts/MovieDetails.jsx
+++ Cinema_Project/scripts/MovieDetails.jsx
@@ -7,42 +7,50 @@ export default class MovieDetails extends React.Component{
     constructor(){
         super();
         this.state = {
-            movieID: "-1"
+            filmTitle: "",
+            filmInfo: "",
+            filmImage: ""
         }
 
     }
 
     componentWillMount(){
-        let filmIndex = this.props.params.key;
-        this.setState({movieID: filmIndex});
-        this.pullFilmInfo();
-    }
-    
-    pullFilmInfo(movieID) {
-        
+        let filmIndex = this.props.params.key;        
+        let movieArr = movieJson.movieDetails;
+        for(let i = 0; i < movieArr.length; i++){
+            if(i == filmIndex){
+                this.setState({filmTitle: movieArr[i].name});
+                this.setState({filmInfo: movieArr[i].description});
+                this.setState({filmImage: movieArr[i].image});
+            }
+        }
     }
     
 
 
     render(){
-
+        let imgLink="../images/"+this.state.filmImage;
+        
         return(
             <div className="container" >
                 <div className="row">
                     <div className="col-sm-12">
-					Title
+					<h3>Movie Details:</h3>
                     </div>
-                    <div className="col-sm-12">Movie Name: {this.state.movieID}</div>
+                    <div className="col-sm-12">Movie Name: {this.state.filmTitle}</div>
                 </div>
                 <div className="row"> 
-                    <div className="col-sm-6" id="rInfo">Movie Info</div>
+                    <div className="col-sm-6" id="rInfo">Movie Info: {this.state.filmInfo}</div>
                     <div className="col-sm-6">
                         <div id="movLoc">
                         </div>
                         <div id="movTime">
                         </div>
                     </div>
-                    <div className="listingTime"><Showings /></div>
+                    <div className="listingTime"><Showings filmID={this.props.params.key}/></div>
+                </div>
+                <div>
+                    <img src={imgLink} />
                 </div>
             </div>
 

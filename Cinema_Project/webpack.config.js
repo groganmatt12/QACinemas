cfeb47f35e4b77fb684248ddc21d937c7e3e8a7c
amhchiu <physicspianist@gmail.com>
2017-07-10 11:07:07 +0100

    webpack with json-laoder

--- Cinema_Project/webpack.config.js
+++ Cinema_Project/webpack.config.js
@@ -1,27 +1,30 @@
 module.exports = {
-  entry: './scripts/main.js',
-  output: {
-    path: __dirname,
-    filename: 'index.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    inline: true,
-    port: 8084
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        use: 'json-loader',
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  }
+    entry: './scripts/main.js',
+    output: {
+        path: __dirname,
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                } 
+            },
+            { 
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+        ]
+    }
 }

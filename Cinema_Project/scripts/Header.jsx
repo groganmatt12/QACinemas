a43fce45de6cc1160ed68fcbf30b6dd89b3ad59a
amhchiu <physicspianist@gmail.com>
2017-07-10 15:50:21 +0100

    stylesheet. navbar updated. px to em

--- Cinema_Project/scripts/Header.jsx
+++ Cinema_Project/scripts/Header.jsx
@@ -6,8 +6,6 @@ export default class Header extends React.Component{
     render(){
         return(
             <div className="static-nav">
-               
-                <h1>QA CINEMA BLUE</h1>    
                 <Navbar /> 
             </div>
         );

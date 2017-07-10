cd3c0e7ed7a2d59f38bda9c6030c842fcacde126
amhchiu <physicspianist@gmail.com>
2017-07-10 14:50:32 +0100

    andrew: got listing gallery with images working

--- Cinema_Project/scripts/ListingGallery.jsx
+++ Cinema_Project/scripts/ListingGallery.jsx
@@ -7,7 +7,7 @@ export default class ListingGallery extends React.Component{
 		super();
 		this.state={
 			movieListings:[]
-		}
+		};
 	}
 	
 	
@@ -35,51 +35,14 @@ export default class ListingGallery extends React.Component{
 		
 		let movieArr = movieJson.movieDetails;
 		let array = this.state.movieListings;
-		for(let i = 0; i<movieArr.length; i++){
-			array.push(
-				<MovieListing name={movieArr[i].name} image={movieArr[i].image} desc={movieArr[i].description} />
-			);
-		}
-/* 
-		let movieArr = movieJson.movieDetails;
-		let array = this.state.movieListings;
 		
 		for(let i = 0; i<movieArr.length; i++){
 			array.push(
-				<div className="row" key={i} >
-					<div className="col-sm-4" id="imgName1">
-						<div id="movImage">
-						{movieArr[i].image}
-						</div>
-						<div id="movName">
-						{movieArr[i].name}
-						</div>
-						
-					</div>
-					<div className="col-sm-8" id="movDesc1">
-						{movieArr[i].description}
-					</div>
-					
-				</div>
+				<MovieListing key={i} name={movieArr[i].name} img={movieArr[i].image} desc={movieArr[i].description} />
 			);
-		} */
-		
-		
-		
-		
-		
-	}
-			/* 	let array= this.state.movieListings;
-		console.log(array[0])
-		for(let i=0; i<10; i++){
-			array.push(
-				<div key={i} style={{backgroundColor:'#00FFFF', width:'auto', height:100}}></div>
-				
-			);
-			
 		}
 		this.setState({movieListings: array});
 		
-	} */
+	}
 
 }

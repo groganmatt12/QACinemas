793bda66853115eeae405ce10bc2308ef58f7c26
amhchiu <physicspianist@gmail.com>
2017-07-11 09:28:50 +0100

    Andrew: updated styling and classnames

--- Cinema_Project/scripts/Home.jsx
+++ Cinema_Project/scripts/Home.jsx
@@ -4,8 +4,11 @@ import React from 'react';
 export default class Home extends React.Component{
     render() {
         return(
-            <div className="homePage">
-				<p>Home Page Content</p>
+            <div className="container">
+			    <div className="row">
+                    testing 
+                    blah blah blah
+                </div>
 			</div>
         );
     }

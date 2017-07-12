7b1470a143918960300d1f8832593c1c50faf381
groganmatt12 <groganmatt@hotmail.co.uk>
2017-07-12 17:43:50 +0100

    matt: navbar done without logic

--- Cinema_Project/src/Home.jsx
+++ Cinema_Project/src/Home.jsx
@@ -16,6 +16,31 @@ export default class Home extends React.Component{
                 <div className="home">
                     <TopFilms />
                 </div>
+                
+                <div className="navbar navbar-default navbar-fixed-bottom" id="quick_book_bar">
+                    <div className="quick_book_bar_container">
+                        <select className="cinema_search_list">
+                            <option>Choose a cinema</option>
+                            <option>Manchester</option>
+                            <option>London</option>
+                        </select>
+                        
+                        <select className="film_search_list">
+                            <option>Choose a film...</option>
+                            <option>Texas Chainsaw Massacre</option>
+                            <option>War for the Planet of the Apes</option>
+                            <option>High School Musical</option>
+                        </select>
+                        
+                        <select>
+                            <option className="time_search_list">Choose a time...</option>
+                            <option className="time_search_list">12:04am</option>
+                            <option className="time_search_list">16:28am</option>
+                            <option className="time_search_list">18:24am</option>
+                            
+                        </select>
+                    </div>
+                </div>
 			</div>
         );
     }

a84ab28ebef9ff3f11d9d57323cbd0d51808bc7e
amhchiu <physicspianist@gmail.com>
2017-07-12 12:03:51 +0100

    working on offers

--- Cinema_Project/src/Offers.jsx
+++ Cinema_Project/src/Offers.jsx
@@ -6,21 +6,30 @@ export default class Offers extends React.Component{
 		return(
 
 
-			<div className="container">
-				<div className="row">
-					<div className="col-sm-12">
-					<p>Best Offers</p>
+			<div className="container" id="offerParent">
+				<div className="row" id="offerHeadings">
+					<div className="col-sm-4" id="mostPopular">
+                        <p>Most Popular</p> 
 					</div>
+                    <div className="col-sm-4">
+                        <p>Family Tickets</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <p>Newest Release</p>
+                    </div>
 				</div>
 				<div className="row">
-					<div className="col-sm-4" id="offer1">
-						<h2>Top Film</h2>
+					<div className="col-sm-4 offerPanels">
+                        <div id="offer1">
+                            <p>Battle Royale</p>
+                        </div>
 					</div>
 					<div className="col-sm-4">
+                         
 					</div>
 
 					<div className="col-sm-4" id="offer2">
-
+                        
 					</div>
 				</div>
 			</div>

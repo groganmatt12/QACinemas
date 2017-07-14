37691543186e8355004c61f270fb1820e71b28f4
KDSherry <kieran.d.sherry@gmail.com>
2017-07-14 09:01:36 +0000

    More JSON data

--- Cinema_Project/src/ListingGallery.jsx
+++ Cinema_Project/src/ListingGallery.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {browserHistory} from 'react-router';
 import CinemaStore from './store/CinemaStore';
 import Sort from './Sort';
 import * as CinemaActions from './actions/CinemaActions';
